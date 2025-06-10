cd5dd2955691bb758e458bdce50e012f2957cf45
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2025-06-10 19:55:02 +0100

    feat: adding api and migration background service architecture tests

---
+++ src/SimplifiedDnd.MigrationService/IMigrationAssemblyMarker.cs
@@ -0,0 +1,7 @@
+namespace SimplifiedDnd.MigrationService;
+
+#pragma warning disable CA1515
+#pragma warning disable CA1040
+public interface IMigrationAssemblyMarker;
+#pragma warning restore CA1040
+#pragma warning restore CA1515
--- test/SimplifiedDnd.ArchitectureTests/LayerTests.cs
+++ test/SimplifiedDnd.ArchitectureTests/LayerTests.cs
@@ -1,23 +1,26 @@
-using System.Reflection;
 using FluentAssertions;
 using NetArchTest.Rules;
 using SimplifiedDnd.Application.Abstractions.Core;
 using SimplifiedDnd.DataBase;
 using SimplifiedDnd.Domain.Characters;
+using SimplifiedDnd.MigrationService;
+using System.Reflection;
 using TestResult = NetArchTest.Rules.TestResult;
 
-namespace SimplifiedDnd.WebApi.ArchitectureTests;
+namespace SimplifiedDnd.ArchitectureTests;
 
 public sealed class LayerTests {
   private static readonly Assembly DomainAssembly = typeof(Character).Assembly;
   private static readonly Assembly ApplicationAssembly = typeof(DomainError).Assembly;
   private static readonly Assembly DataBaseAssembly = typeof(DataBaseDependencyInjection).Assembly;
-  private static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+  private static readonly Assembly ApiAssembly = typeof(Program).Assembly;
+  private static readonly Assembly MigrationAssembly = typeof(IMigrationAssemblyMarker).Assembly;
 
   private sealed class UnexpectedDomainDependencies() : TheoryData<string>(
     ApplicationAssembly.GetName().Name!,
     DataBaseAssembly.GetName().Name!,
-    PresentationAssembly.GetName().Name!);
+    ApiAssembly.GetName().Name!,
+    MigrationAssembly.GetName().Name!);
 
   [Theory(DisplayName = "Domain layer doesn't depend on layer")]
   [ClassData(typeof(UnexpectedDomainDependencies))]
@@ -34,7 +37,8 @@ public sealed class LayerTests {
   
   private sealed class UnexpectedApplicationDependencies() : TheoryData<string>(
     DataBaseAssembly.GetName().Name!,
-    PresentationAssembly.GetName().Name!);
+    ApiAssembly.GetName().Name!,
+    MigrationAssembly.GetName().Name!);
   
   [Theory(DisplayName = "Application doesn't depend on layer")]
   [ClassData(typeof(UnexpectedApplicationDependencies))]
@@ -50,13 +54,47 @@ public sealed class LayerTests {
   }
   
   private sealed class UnexpectedDataBaseDependencies() : TheoryData<string>(
-    PresentationAssembly.GetName().Name!);
+    ApiAssembly.GetName().Name!,
+    MigrationAssembly.GetName().Name!);
   
   [Theory(DisplayName = "Data base layer doesn't depends on layer")]
   [ClassData(typeof(UnexpectedDataBaseDependencies))]
   public void DataBaseDoesNotHaveSpecifiedDependency(string assemblyName) {
     // Arrange & Act
     TestResult? result = Types.InAssembly(DataBaseAssembly)
+      .ShouldNot()
+      .HaveDependencyOn(assemblyName)
+      .GetResult();
+
+    // Assert
+    result.IsSuccessful.Should().BeTrue();
+  }
+  
+  private sealed class UnexpectedApiDependencies() : TheoryData<string>(
+    MigrationAssembly.GetName().Name!);
+  
+  [Theory(DisplayName = "API doesn't depends on layer")]
+  [ClassData(typeof(UnexpectedApiDependencies))]
+  public void ApiDoesNotHaveSpecifiedDependency(string assemblyName) {
+    // Arrange & Act
+    TestResult? result = Types.InAssembly(ApiAssembly)
+      .ShouldNot()
+      .HaveDependencyOn(assemblyName)
+      .GetResult();
+
+    // Assert
+    result.IsSuccessful.Should().BeTrue();
+  }
+  
+  
+  private sealed class UnexpectedMigrationDependencies() : TheoryData<string>(
+    MigrationAssembly.GetName().Name!);
+  
+  [Theory(DisplayName = "Migration background service doesn't depends on layer")]
+  [ClassData(typeof(UnexpectedMigrationDependencies))]
+  public void MigrationDoesNotHaveSpecifiedDependency(string assemblyName) {
+    // Arrange & Act
+    TestResult? result = Types.InAssembly(MigrationAssembly)
       .ShouldNot()
       .HaveDependencyOn(assemblyName)
       .GetResult();
